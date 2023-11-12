30eb1ce8c2f5da346a7dc4f9fc2344f0c7cc9e22
unknown <aliemre__kskyl@hotmail.com>
2023-11-12 11:01:43 +0300

    html sayfama input ekledim js ile placeholderi ekledik

---
+++ script.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < countries.length; i++) {
+    console.log(countries[i] ['name']);
+}
+
+
+
+const input = document.getElementById('input')
+
+input.setAttribute('placeholder', 'Değerim')
