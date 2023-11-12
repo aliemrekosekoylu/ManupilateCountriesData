a36d9918f33a09bb7976feb3a42dd9b8dc884a6a
unknown <aliemre__kskyl@hotmail.com>
2023-11-12 12:09:30 +0300

    countriesDatayı for ile döndürdük

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 for (let i = 0; i < countries.length; i++) {
+    const p = document.createElement('p')
+    p.style.color = 'red'
+    p.innerHTML = countries[i] ['name']
     console.log(countries[i] ['name']);
+
+    document.body.append(p)
 }
-
-
-
 const input = document.getElementById('input')
+input.addEventListener('keyup', (Element) =>{
+    console.log(Element.key)
+    console.log(Element.keyCode)
+    if (Element.keyCode ==13) {
+        countries.filter((ulke) => {
+            if(ulke.name == input.value) {
+                const p = document.createElement('p')
+                p.style.color = 'blue'
+                p.innerHTML = `${ulke.name} - ${ulke.capital}`
+                document.body.append(p)
+            }
+        })
+    }
+
+ 
+})
+
+
+
+
 
 input.setAttribute('placeholder', 'Değerim')
 
@@ -15,4 +37,6 @@ div.style.border = "1px solid black"
 
 
 document.body.appendChild(div)
+
+document.body.removeChild(div)
 
